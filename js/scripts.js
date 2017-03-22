90d23ee1401653fbca5646f11a4fd576c34fe3f4
mickijames <micalynjeanette@eagles.ewu.edu>
2017-03-22 00:26:52 -0700

    Added Color & Forecast Days
    * Moved weather display to a function
    * Added color to h1 and icons
    * Added 2 days to the forecast

--- js/scripts.js
+++ js/scripts.js
@@ -7,48 +7,11 @@ $.simpleWeather({
         // Entire weather object
         console.log(weather);
 
-        // Display Today's Weather
-        $('.city').text(weather.city);
-        $('.state').text(weather.region);
-        $('.temp').text(weather.temp);
-        $('i').addClass('icon-' + weather.code);
+        displayWeather(weather);
         
         background(weather);
         
-        // Forecast Day 1
-        $('section figure:nth-child(1) h2').text(weather.forecast[0].day);
-        $('section figure:nth-child(1) i').addClass('icon-' + weather.forecast[0].code);
-        $('section figure:nth-child(1) figcaption').text(weather.forecast[0].text);
-        $('section figure:nth-child(1) .high').text(weather.forecast[0].high);
-        $('section figure:nth-child(1) .low').text(weather.forecast[0].low);
-
-        // Forecast Day 2
-        $('section figure:nth-child(2) h2').text(weather.forecast[1].day);
-        $('section figure:nth-child(2) i').addClass('icon-' + weather.forecast[1].code);
-        $('section figure:nth-child(2) figcaption').text(weather.forecast[1].text);
-        $('section figure:nth-child(2) .high').text(weather.forecast[1].high);
-        $('section figure:nth-child(2) .low').text(weather.forecast[1].low);
-
-        // Forecast Day 3
-        $('section figure:nth-child(3) h2').text(weather.forecast[2].day);
-        $('section figure:nth-child(3) i').addClass('icon-' + weather.forecast[2].code);
-        $('section figure:nth-child(3) figcaption').text(weather.forecast[2].text);
-        $('section figure:nth-child(3) .high').text(weather.forecast[2].high);
-        $('section figure:nth-child(3) .low').text(weather.forecast[2].low);
-
-        // Forecast Day 4
-        $('section figure:nth-child(4) h2').text(weather.forecast[3].day);
-        $('section figure:nth-child(4) i').addClass('icon-' + weather.forecast[3].code);
-        $('section figure:nth-child(4) figcaption').text(weather.forecast[3].text);
-        $('section figure:nth-child(4) .high').text(weather.forecast[3].high);
-        $('section figure:nth-child(4) .low').text(weather.forecast[3].low);
-
-        // Forecast Day 5
-        $('section figure:nth-child(5) h2').text(weather.forecast[4].day);
-        $('section figure:nth-child(5) i').addClass('icon-' + weather.forecast[4].code);
-        $('section figure:nth-child(5) figcaption').text(weather.forecast[4].text);
-        $('section figure:nth-child(5) .high').text(weather.forecast[4].high);
-        $('section figure:nth-child(5) .low').text(weather.forecast[4].low);
+        forecast(weather);
       
     },
     error: function (error) {
@@ -57,6 +20,65 @@ $.simpleWeather({
     }
  
 });
+
+var displayWeather = function (weather) {
+    // Display Today's Weather
+    $('.city').text(weather.city);
+    $('.state').text(weather.region);
+    $('.temp').text(weather.temp);
+    $('i').addClass('icon-' + weather.code);
+};
+
+var forecast = function (weather) {
+    // Forecast Day 1
+    $('section figure:nth-child(1) h2').text(weather.forecast[0].day);
+    $('section figure:nth-child(1) i').addClass('icon-' + weather.forecast[0].code);
+    $('section figure:nth-child(1) figcaption').text(weather.forecast[0].text);
+    $('section figure:nth-child(1) .high').text(weather.forecast[0].high);
+    $('section figure:nth-child(1) .low').text(weather.forecast[0].low);
+
+    // Forecast Day 2
+    $('section figure:nth-child(2) h2').text(weather.forecast[1].day);
+    $('section figure:nth-child(2) i').addClass('icon-' + weather.forecast[1].code);
+    $('section figure:nth-child(2) figcaption').text(weather.forecast[1].text);
+    $('section figure:nth-child(2) .high').text(weather.forecast[1].high);
+    $('section figure:nth-child(2) .low').text(weather.forecast[1].low);
+
+    // Forecast Day 3
+    $('section figure:nth-child(3) h2').text(weather.forecast[2].day);
+    $('section figure:nth-child(3) i').addClass('icon-' + weather.forecast[2].code);
+    $('section figure:nth-child(3) figcaption').text(weather.forecast[2].text);
+    $('section figure:nth-child(3) .high').text(weather.forecast[2].high);
+    $('section figure:nth-child(3) .low').text(weather.forecast[2].low);
+
+    // Forecast Day 4
+    $('section figure:nth-child(4) h2').text(weather.forecast[3].day);
+    $('section figure:nth-child(4) i').addClass('icon-' + weather.forecast[3].code);
+    $('section figure:nth-child(4) figcaption').text(weather.forecast[3].text);
+    $('section figure:nth-child(4) .high').text(weather.forecast[3].high);
+    $('section figure:nth-child(4) .low').text(weather.forecast[3].low);
+
+    // Forecast Day 5
+    $('section figure:nth-child(5) h2').text(weather.forecast[4].day);
+    $('section figure:nth-child(5) i').addClass('icon-' + weather.forecast[4].code);
+    $('section figure:nth-child(5) figcaption').text(weather.forecast[4].text);
+    $('section figure:nth-child(5) .high').text(weather.forecast[4].high);
+    $('section figure:nth-child(5) .low').text(weather.forecast[4].low);
+
+    // Forecast Day 6
+    $('section figure:nth-child(6) h2').text(weather.forecast[5].day);
+    $('section figure:nth-child(6) i').addClass('icon-' + weather.forecast[5].code);
+    $('section figure:nth-child(6) figcaption').text(weather.forecast[5].text);
+    $('section figure:nth-child(6) .high').text(weather.forecast[5].high);
+    $('section figure:nth-child(6) .low').text(weather.forecast[5].low);
+
+    // Forecast Day 7
+    $('section figure:nth-child(7) h2').text(weather.forecast[6].day);
+    $('section figure:nth-child(7) i').addClass('icon-' + weather.forecast[6].code);
+    $('section figure:nth-child(7) figcaption').text(weather.forecast[6].text);
+    $('section figure:nth-child(7) .high').text(weather.forecast[6].high);
+    $('section figure:nth-child(7) .low').text(weather.forecast[6].low);
+};
 
 var background = function (weather) {
     // Sunny Weather BG
